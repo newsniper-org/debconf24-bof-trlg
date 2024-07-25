a5c3fa4647bc46a76db5398e3d1fcf52700d9ceb
윤병익 <yeun0908@gmail.com>
2024-07-26 02:48:08 +0900

    further cleanup

--- src/consts.ts
+++ src/consts.ts
@@ -1,14 +1,12 @@
-import type { Metadata, Site, Socials } from "@/types";
+import type { Metadata, Site } from "@/types";
 
 export const SITE: Site = {
-  TITLE: "NEWSNIPER",
-  DESCRIPTION: "NEWSNIPER 공식 사이트입니다.",
-  EMAIL: "newsniper@protonmail.com",
-  NUM_POSTS_ON_HOMEPAGE: 5,
-  NUM_PROJECTS_ON_HOMEPAGE: 3,
+  TITLE: "NEWSNIPER DebConf24 BoF Session - the Revised Landlord's Game",
+  DESCRIPTION: "<두 개의 세상> 웹용 이식판 with 희년함께",
+  EMAIL: "newsniper@protonmail.com"
 };
 
 export const HOME: Metadata = {
   TITLE: "NEWSNIPER DebConf24 BoF Session - the Revised Landlord's Game",
-  DESCRIPTION: "with 희년함께",
+  DESCRIPTION: "<두 개의 세상> 웹용 이식판 with 희년함께",
 };
