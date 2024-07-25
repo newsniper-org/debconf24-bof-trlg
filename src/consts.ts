ad1596d0801f3d4d0d68e797b1ce533b4b2e8da1
윤병익 <yeun0908@gmail.com>
2024-07-26 02:52:19 +0900

    fix ui

--- src/consts.ts
+++ src/consts.ts
@@ -1,8 +1,8 @@
 import type { Metadata, Site } from "@/types";
 
 export const SITE: Site = {
-  TITLE: "NEWSNIPER DebConf24 BoF Session - the Revised Landlord's Game",
-  DESCRIPTION: "<두 개의 세상> 웹용 이식판 with 희년함께",
+  TITLE: "NEWSNIPER",
+  DESCRIPTION: "NEWSNIPER",
   EMAIL: "newsniper@protonmail.com"
 };
 
